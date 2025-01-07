168d66ea8a96734f40e1221333c3e22bef67798b
Tai Nguyen <tai.nguyenvan2@smartdev.com>
2025-01-07 13:01:39 +0700

    set base for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/bean-reader-json/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
